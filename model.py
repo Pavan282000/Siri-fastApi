f2a8171f3b1af24792e22cc00453e917eb409d78
PAVAN KUMAR POTHALA <53993242+Pavan282000@users.noreply.github.com>
2022-02-14 16:02:25 +0530

    Update model.py

--- model.py
+++ model.py
@@ -11,6 +11,182 @@ import uvicorn as uvicorn
 import numpy as np
 
 
+cutoff = {
+        "Shop Floor Automation" : 2.5,
+"verticalIntegration":2.5,
+"horizontalIntegration" :2.5,
+"Enterprise Automation" :2.5,
+"Facility Automation" :2.5,
+"ProductLifeCycle" : 2.5,
+"Shop Floor Connectivityn" : 2.5,
+"Enterprise Connectivityn" : 2.5,
+" Facility Connectivity": 2.5,
+"Shop Floor Intelligence" : 2.5,
+"Enterprise Intelligence": 2.5,
+"Facility Intelligence": 2.5,
+"Leadership Competency": 2.5,
+"Workforce Learning & Development ": 2.5,
+"Strategy & governance": 2.5,
+"Inter- & intra-company collaboration": 2.5,
+    }
+weight = {
+        "Shop Floor Automation" :0.111,
+"verticalIntegration": 0.111,
+"horizontalIntegration" : 0.111,
+"Enterprise Automation" : 0.037,
+"Facility Automation" :0.037,
+"ProductLifeCycle" :0.037,
+"Shop Floor Connectivityn" :0.037,
+"Enterprise Connectivityn" :0.037,
+" Facility Connectivity":0.037,
+"Shop Floor Intelligence" :0.037,
+"Enterprise Intelligence": 0.037,
+"Facility Intelligence": 0.037,
+"Leadership Competency": 0.08325,
+"Workforce Learning & Development ":0.08325,
+"Strategy & governance":0.08325,
+"Inter- & intra-company collaboration":0.08325,
+    }
+
+class MPI:
+    table = {}
+    cutoff = 2.5
+
+    def __init__(self, values, CompanyName):
+        self.d = values
+        self.name = CompanyName
+        MPI.table[self.name] = {}
+        for i in self.d:
+            MPI.table[self.name][i['Dim']] = i['value']
+
+    @staticmethod
+    def findDeprivation_matrix():
+        dep_matrix = pd.DataFrame(MPI.table)
+        for company in dep_matrix.columns:
+            for dimension in dep_matrix.index:
+                value = dep_matrix[company].loc[dimension]  # provide weight here
+                ###Provide cut off
+                if (value >= cutoff[dimension]):
+                    dep_matrix[company].loc[dimension] = 0
+                else:
+                    dep_matrix[company].loc[dimension] = 1  # provide weight here
+        return dep_matrix
+
+    @staticmethod
+    def total_Cj():
+        dep_matrix = MPI.findDeprivation_matrix()
+        totalCj = pd.DataFrame(dep_matrix.sum(), columns=["Total"])  # Cj Vector
+        count = 0
+        cut = 6
+        print(totalCj)
+        for i in totalCj.index:
+            if totalCj.loc[i]['Total'] >= cut:  # If company totalCj is greater than cut
+                count = count + 1
+            else:
+                totalCj.loc[i]['Total'] = 0
+                for j in dep_matrix.index:
+                    dep_matrix.loc[j][i] = 0
+        #         print(count)
+        HeadCount = count / len(dep_matrix.columns)
+        print("HeadCount", HeadCount)
+        return totalCj, dep_matrix, HeadCount
+
+    @staticmethod
+    def deprivation_share():
+        k = MPI.total_Cj()
+        total = k[0]
+        dep_matrix = k[1]
+        share = {}
+        for i in total.index:
+            value = total.loc[i]['Total'] / len(dep_matrix.index)
+            share[i] = value
+        print(share)
+        return share
+
+    @staticmethod
+    def adjusted_headCount():
+        k = MPI.total_Cj()
+        total_Cj = k[0]
+        dep_matrix = k[1]
+        HeadCount = k[2]
+        total = 0
+        count = 0
+        #         print(total_Cj)
+        for i in total_Cj.index:
+            if (total_Cj.loc[i]['Total'] > 0):
+                share = total_Cj.loc[i]['Total'] / len(dep_matrix.index)
+                #                 print("share" , share)
+                total = total + share
+                count = count + 1
+        #         print(total)
+        average = (total / count)
+        #         print(average)
+        adjusted_headCount = HeadCount * average
+        #         print(adjusted_headCount)
+        return adjusted_headCount, dep_matrix
+
+    @staticmethod
+    def UnCensored_HeadCountRatio():
+        k = MPI.adjusted_headCount()
+        dep_matrix = k[1]
+        #         adjusted_headCount = k[0]
+        trans = dep_matrix.transpose()
+        print(trans)
+        trans = pd.DataFrame(trans.sum(), columns=["Total"])
+        print(trans)
+        Uh = []
+        for i in trans.index:
+            Uh.append(trans.loc[i]['Total'] / len(trans.index))
+        print(Uh)
+        return Uh
+
+    @staticmethod
+    def Censored_HeadCountRatio():
+        k = MPI.adjusted_headCount()
+        dep_matrix = k[1]
+
+        for company in dep_matrix.columns:
+            for dimension in dep_matrix.index:
+                dep_matrix[company].loc[dimension] = dep_matrix[company].loc[dimension] * weight[dimension]
+        print(dep_matrix)
+        total = pd.DataFrame(dep_matrix.sum(), columns=["Total"])
+        cutoff = 0.5
+        for i in total.index:
+            if (total.loc[i]['Total'] < cutoff):
+                dep_matrix.drop(i, inplace=True, axis=1)
+        print(total)
+        t = dep_matrix.transpose()  # Companies that has Cj greater than zero
+        print(t)
+        total2 = pd.DataFrame(t.sum(), columns=["Total"])  # calculating the sum for unfit companies
+        #         print(total2)
+        Ch = {}
+
+        for i in t.columns:
+            count = 0
+            for j in t.index:
+                if (t.loc[j][i] != 0):
+                    count = count + 1
+            Ch[i] = (count / len(total2.index))
+        return Ch
+
+    @staticmethod
+    def M0():
+        k = MPI.Censored_HeadCountRatio()
+        dep = MPI.findDeprivation_matrix()
+        s = 0
+        for i in dep.index:
+            s = s + k[i] * (weight[i] / len(dep.index))
+        new_adjusted_headcount = s
+        return new_adjusted_headcount
+
+
+
+
+
+
+
+
+
 app = FastAPI()
 
 app.add_middleware(
@@ -23,6 +199,353 @@ app.add_middleware(
 
 graphdb = GraphDatabase.driver(uri="neo4j+s://f951f243.databases.neo4j.io", auth=("neo4j", "08_xJvvomlHaYsaUnUfXiM8hFQmvRijMzPDed63f_y0"), max_connection_lifetime=1)
 session = graphdb.session()
+
+
+
+
+com1 = [
+    {
+        "Dim": "Shop Floor Automation",
+        "value": 4
+    },
+    {
+        "Dim": "verticalIntegration",
+        "value": 0
+    },
+    {
+        "Dim": "horizontalIntegration",
+        "value": 5
+    },
+    {
+        "Dim": "Enterprise Automation",
+        "value": 4
+    },
+    {
+        "Dim": "Facility Automation",
+        "value": 1
+    },
+    {
+        "Dim": "ProductLifeCycle",
+        "value": 5
+    },
+    {
+        "Dim": "Shop Floor Connectivityn",
+        "value": 0
+    },
+    {
+        "Dim": "Enterprise Connectivityn",
+        "value": 1
+    },
+    {
+        "Dim": " Facility Connectivity",
+        "value": 1
+    },
+    {
+        "Dim": "Shop Floor Intelligence",
+        "value": 1
+    },
+    {
+        "Dim": "Enterprise Intelligence",
+        "value": 0
+    },
+    {
+        "Dim": "Facility Intelligence",
+        "value": 4
+    },
+    {
+        "Dim": "Leadership Competency",
+        "value": 0
+    },
+    {
+        "Dim": "Workforce Learning & Development ",
+        "value": 3
+    },
+    {
+        "Dim": "Strategy & governance",
+        "value": 1
+    },
+    {
+        "Dim": "Inter- & intra-company collaboration",
+        "value": 0
+    }
+]
+com2 = [
+    {
+        "Dim": "Shop Floor Automation",
+        "value": 0
+    },
+    {
+        "Dim": "verticalIntegration",
+        "value": 2
+    },
+    {
+        "Dim": "horizontalIntegration",
+        "value": 4
+    },
+    {
+        "Dim": "Enterprise Automation",
+        "value": 0
+    },
+    {
+        "Dim": "Facility Automation",
+        "value": 1
+    },
+    {
+        "Dim": "ProductLifeCycle",
+        "value": 0
+    },
+    {
+        "Dim": "Shop Floor Connectivityn",
+        "value": 2
+    },
+    {
+        "Dim": "Enterprise Connectivityn",
+        "value": 3
+    },
+    {
+        "Dim": " Facility Connectivity",
+        "value": 5
+    },
+    {
+        "Dim": "Shop Floor Intelligence",
+        "value": 5
+    },
+    {
+        "Dim": "Enterprise Intelligence",
+        "value": 0
+    },
+    {
+        "Dim": "Facility Intelligence",
+        "value": 2
+    },
+    {
+        "Dim": "Leadership Competency",
+        "value": 5
+    },
+    {
+        "Dim": "Workforce Learning & Development ",
+        "value": 1
+    },
+    {
+        "Dim": "Strategy & governance",
+        "value": 1
+    },
+    {
+        "Dim": "Inter- & intra-company collaboration",
+        "value": 4
+    }
+]
+com3 = [
+    {
+        "Dim": "Shop Floor Automation",
+        "value": 3
+    },
+    {
+        "Dim": "verticalIntegration",
+        "value": 0
+    },
+    {
+        "Dim": "horizontalIntegration",
+        "value": 5
+    },
+    {
+        "Dim": "Enterprise Automation",
+        "value": 1
+    },
+    {
+        "Dim": "Facility Automation",
+        "value": 1
+    },
+    {
+        "Dim": "ProductLifeCycle",
+        "value": 4
+    },
+    {
+        "Dim": "Shop Floor Connectivityn",
+        "value": 5
+    },
+    {
+        "Dim": "Enterprise Connectivityn",
+        "value": 5
+    },
+    {
+        "Dim": " Facility Connectivity",
+        "value": 1
+    },
+    {
+        "Dim": "Shop Floor Intelligence",
+        "value": 4
+    },
+    {
+        "Dim": "Enterprise Intelligence",
+        "value": 3
+    },
+    {
+        "Dim": "Facility Intelligence",
+        "value": 5
+    },
+    {
+        "Dim": "Leadership Competency",
+        "value": 4
+    },
+    {
+        "Dim": "Workforce Learning & Development ",
+        "value": 0
+    },
+    {
+        "Dim": "Strategy & governance",
+        "value": 0
+    },
+    {
+        "Dim": "Inter- & intra-company collaboration",
+        "value": 2
+    }
+]
+com4 = [
+    {
+        "Dim": "Shop Floor Automation",
+        "value": 1
+    },
+    {
+        "Dim": "verticalIntegration",
+        "value": 1
+    },
+    {
+        "Dim": "horizontalIntegration",
+        "value": 5
+    },
+    {
+        "Dim": "Enterprise Automation",
+        "value": 1
+    },
+    {
+        "Dim": "Facility Automation",
+        "value": 5
+    },
+    {
+        "Dim": "ProductLifeCycle",
+        "value": 2
+    },
+    {
+        "Dim": "Shop Floor Connectivityn",
+        "value": 0
+    },
+    {
+        "Dim": "Enterprise Connectivityn",
+        "value": 0
+    },
+    {
+        "Dim": " Facility Connectivity",
+        "value": 4
+    },
+    {
+        "Dim": "Shop Floor Intelligence",
+        "value": 0
+    },
+    {
+        "Dim": "Enterprise Intelligence",
+        "value": 5
+    },
+    {
+        "Dim": "Facility Intelligence",
+        "value": 1
+    },
+    {
+        "Dim": "Leadership Competency",
+        "value": 4
+    },
+    {
+        "Dim": "Workforce Learning & Development ",
+        "value": 2
+    },
+    {
+        "Dim": "Strategy & governance",
+        "value": 4
+    },
+    {
+        "Dim": "Inter- & intra-company collaboration",
+        "value": 3
+    }
+]
+
+
+@app.get('/insert' ,tags=["DataFrame"])
+async def insert():
+    # orgName: str, sname: str
+ #    q2 = '''match(o:Organisation{name:$orgName})-[:hadSurvey]->(s:SurveyedModel{name:$sname})-[:hasValue]->(v:Value) return v.name as Dim,v.value as value
+ # '''
+ #    x = {"orgName":orgName,"sname":sname}
+ #    result = session.run(q2,x)
+ #    data = result.data()
+ #    json_data = jsonable_encoder(data)
+
+    MPI(com1,"Com1")
+    MPI(com2,"Com2")
+    MPI(com3,"Com3")
+    MPI(com4,"Com4")
+
+    return "Done"
+
+
+@app.get('/getAdjustedHeadCount' ,tags=["DataFrame"])
+async def adjustHeacount():
+    # orgName: str, sname: str
+ #    q2 = '''match(o:Organisation{name:$orgName})-[:hadSurvey]->(s:SurveyedModel{name:$sname})-[:hasValue]->(v:Value) return v.name as Dim,v.value as value
+ # '''
+ #    x = {"orgName":orgName,"sname":sname}
+ #    result = session.run(q2,x)
+ #    data = result.data()
+ #    json_data = jsonable_encoder(data)
+
+
+    k = MPI.adjusted_headCount()
+    m = k[0]
+    return m
+
+
+@app.get('/getUncensoredHeadCount' ,tags=["DataFrame"])
+async def Uncensored():
+    # orgName: str, sname: str
+ #    q2 = '''match(o:Organisation{name:$orgName})-[:hadSurvey]->(s:SurveyedModel{name:$sname})-[:hasValue]->(v:Value) return v.name as Dim,v.value as value
+ # '''
+ #    x = {"orgName":orgName,"sname":sname}
+ #    result = session.run(q2,x)
+ #    data = result.data()
+ #    json_data = jsonable_encoder(data)
+
+
+    k = MPI.UnCensored_HeadCountRatio()
+    return k
+
+
+@app.get('/getCensoredHeadCount' ,tags=["DataFrame"])
+async def censored():
+    # orgName: str, sname: str
+ #    q2 = '''match(o:Organisation{name:$orgName})-[:hadSurvey]->(s:SurveyedModel{name:$sname})-[:hasValue]->(v:Value) return v.name as Dim,v.value as value
+ # '''
+ #    x = {"orgName":orgName,"sname":sname}
+ #    result = session.run(q2,x)
+ #    data = result.data()
+ #    json_data = jsonable_encoder(data)
+    k = MPI.Censored_HeadCountRatio()
+    return k
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @app.post('/addParentNode',tags=["Organisation"])
 async def addParentNode():
