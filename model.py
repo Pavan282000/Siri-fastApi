5f497cf6d261ce5718c866589ae2d9f58c155e0b
PAVAN KUMAR POTHALA <53993242+Pavan282000@users.noreply.github.com>
2022-03-01 19:34:53 +0530

    Update model.py

--- model.py
+++ model.py
@@ -215,8 +215,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-graphdb = GraphDatabase.driver(uri="bolt://localhost:7687", auth=("neo4j", "yash"), max_connection_lifetime=1)
-# graphdb = GraphDatabase.driver(uri="neo4j+s://f951f243.databases.neo4j.io", auth=("neo4j", "08_xJvvomlHaYsaUnUfXiM8hFQmvRijMzPDed63f_y0"), max_connection_lifetime=1)
+# graphdb = GraphDatabase.driver(uri="bolt://localhost:7687", auth=("neo4j", "yash"), max_connection_lifetime=1)
+graphdb = GraphDatabase.driver(uri="neo4j+s://f951f243.databases.neo4j.io", auth=("neo4j", "08_xJvvomlHaYsaUnUfXiM8hFQmvRijMzPDed63f_y0"), max_connection_lifetime=1)
 session = graphdb.session()
 
 
